9e28b2ea32dd47232debebebff769f2e9dea7744
William Zhao <williezhao123@gmail.com>
2026-07-08 09:39:41 -0700

    Added link rich preview

---
+++ src/_data/site.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: "William Zhao's Portfolio",
+	shortName: "William Zhao",
+	description:
+		"Currently a Gameplay Engineer Intern @ E-Line Media.",
+	author: "William Zhao",
+	locale: "en_US",
+	themeColor: "#007BEE",
+	url: "https://william-zhao.com",
+	image: "/about/media/Cover.jpg",
+};
